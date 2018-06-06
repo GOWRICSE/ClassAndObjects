d5930f3789e2e8dc70db9ecabcbb4f93670b720a
GOWRI <31369935+GOWRICSE@users.noreply.github.com>
2018-06-06 16:47:19 +0530

    Add files via upload

---
+++ Box.java
@@ -0,0 +1,12 @@
+public class Box 
+{
+	static double Volume(double w,double h,double d)
+	{
+		return (w*h*d);
+	}
+	public static void main(String[] args) 
+	{
+	System.out.println(Volume(10,12,14));	
+	}
+	
+}
